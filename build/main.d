1d1c5a8d2826c51e7bcc815873cf2b5d2178428a
Teotia444 <teotia444.video@gmail.com>
2020-04-13 15:10:10 +0200

    Pre-release

---
+++ build/main.d
@@ -0,0 +1,242 @@
+main.o: \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/examples/3ds/templates/application/source/main.c \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/types.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/result.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/ipc.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/svc.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/exheader.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/srv.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/errf.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/os.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/synchronization.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/thread.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gfx.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/gspgpu.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/console.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/env.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/util/decompress.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/util/utf.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/allocator/linear.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/allocator/mappable.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/allocator/vram.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ac.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/am.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/fs.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ampxi.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/apt.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/boss.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/cam.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/y2r.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/cfgnor.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/cfgu.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/csnd.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/dsp.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/fsreg.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/frd.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/mii.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/gsplcd.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/hid.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/irrst.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/sslc.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/httpc.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/uds.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ndm.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/nim.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/nwmext.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ir.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ns.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/pmapp.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/pmdbg.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ps.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ptmu.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ptmsysm.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/pxidev.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/pxipm.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/soc.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/netinet/in.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/sys/socket.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/mic.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/mvd.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/nfc.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/news.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/qtm.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/srvpm.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/loader.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/mcuhwc.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/hb.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/gx.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/gpu.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/registers.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/enums.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/shbin.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/shaderProgram.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/ndsp/ndsp.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/ndsp/channel.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/applets/swkbd.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/applets/error.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/applets/miiselector.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/sdmc.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/romfs.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/font.h \
+ C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gdbhio_dev.h
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/types.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/result.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/ipc.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/svc.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/exheader.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/srv.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/errf.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/os.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/synchronization.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/thread.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gfx.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/gspgpu.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/console.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/env.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/util/decompress.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/util/utf.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/allocator/linear.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/allocator/mappable.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/allocator/vram.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ac.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/am.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/fs.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ampxi.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/apt.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/boss.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/cam.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/y2r.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/cfgnor.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/cfgu.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/csnd.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/dsp.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/fsreg.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/frd.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/mii.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/gsplcd.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/hid.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/irrst.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/sslc.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/httpc.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/uds.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ndm.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/nim.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/nwmext.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ir.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ns.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/pmapp.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/pmdbg.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ps.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ptmu.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/ptmsysm.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/pxidev.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/pxipm.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/soc.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/netinet/in.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/sys/socket.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/mic.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/mvd.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/nfc.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/news.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/qtm.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/srvpm.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/loader.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/mcuhwc.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/services/hb.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/gx.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/gpu.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/registers.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/enums.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/shbin.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gpu/shaderProgram.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/ndsp/ndsp.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/ndsp/channel.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/applets/swkbd.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/applets/error.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/applets/miiselector.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/sdmc.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/romfs.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/font.h:
+
+C:/Users/compt/AppData/Roaming/devkit/devkitPro/libctru/include/3ds/gdbhio_dev.h:
